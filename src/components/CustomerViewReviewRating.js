e6b43cc47bd7e5b0add785fda8f125ccf4d34ea4
IceDeathEx <taneekongnicholas@gmail.com>
2023-05-17 13:00:17 +0800

    Upgraded it to a functional UI
    Upgraded the existing Review and Rating for Customer by providing the fetch logic to retrieve the review and rating list.
    
    Things left to do: Integrate with main code and tidying up on UI

--- src/components/CustomerViewReviewRating.js
+++ src/components/CustomerViewReviewRating.js
@@ -1,18 +1,32 @@
+import { useEffect, useState } from "react";
 const CustomerViewReviewRating = () => {
-    return ( 
-        <form>
-            <h1>Review and Rating</h1>
-            <p>This cinema is very cozy.</p>
-            <p>Rating 4/5</p>
-            <p>This cinema is very cozy.</p>
-            <p>Rating 3/5</p>
-            <p>This cinema is very cozy.</p>
-            <p>Rating 2/5</p>
-            <p>This cinema is very cozy.</p>
-            <p>Rating 1/5</p>
-            <p>This cinema is very cozy.</p>
-            <p>Rating 5/5</p>
-        </form>
+  const [reviews, setReviews] = useState(null);
+    useEffect(() => {
+        fetch('http://localhost:8000/reviews')
+        .then(res =>{
+            return res.json();
+        })
+        .then(data => {
+            setReviews(data);
+        })
+    }, []);  
+    const handleClick =((e) => {
+      fetch('http://localhost:8000/reviews/' + e.target.value, {
+        method: 'DELETE'
+      })
+    })
+
+  return ( 
+        <div className="review-list">
+          <h1>Review and Rating Section</h1>
+      {reviews && reviews.map(review => (
+        <div className="review-preview" key={review.id} >
+            <p>{ review.feedback }</p>
+            <p>{ review.rating }</p>
+            <button value={review.id} onClick={handleClick}>Delete</button>
+        </div>
+      ))}
+    </div>
      );
 }
  
